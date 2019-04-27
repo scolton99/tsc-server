ca9e8397ef9fb39394edab6e9869bb447082d8ea
Spencer Colton <me@scolton.tech>
2019-04-27 15:34:29 -0500

    Debugging part 2

--- routes/kudos.js
+++ routes/kudos.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   console.dir(req.body);
+
+  const {channel: {id: channel}, message: {ts: message_ts}} = req.body.payload;
+  const api_token = process.env.SLACK_API_TOKEN;
+  const fetch_response = await request(`https://slack.com/api/conversations.replies?token=${api_token}&channel=${channel}&ts=${message_ts}`);
+
+  console.dir(fetch_response);
 });
 
 module.exports = router;
