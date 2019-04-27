53bedf025112fc572fbeb0d82b5be46b19e11af3
Spencer Colton <me@scolton.tech>
2019-04-27 17:23:44 -0500

    More debugging

--- routes/kudos.js
+++ routes/kudos.js
@@ -1,16 +1,78 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request-promise');
+var airtable = require('airtable');
+var a_base = new airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appydp8wFv8Yd5nVE');
 
 router.post('/', async (req, res) => {
-  const {channel: {id: channel}, message: {ts: message_ts}} = JSON.parse(req.body.payload);
+  const {
+    channel: {
+      id: channel
+    },
+    message: {
+      ts: message_ts,
+      blocks
+    },
+    actions: [
+      {
+        value: action_data
+      }
+    ],
+    user: {
+      username
+    }
+  } = JSON.parse(req.body.payload);
+  console.dir(req.body);
+
   const api_token = process.env.SLACK_API_TOKEN;
+  const [action_value, record_id] = action_data.split("_");
+
+  let fetch_response = null;
+  let display_text = null;
   try {
-    const fetch_response = await request(`https://slack.com/api/conversations.replies?token=${api_token}&channel=${channel}&ts=${message_ts}`);
-    console.dir(fetch_response);
+    fetch_response = await request(`https://slack.com/api/conversations.replies?token=${api_token}&channel=${channel}&ts=${message_ts}`);
+    const { messages } = JSON.parse(fetch_response);
+
+    if (messages.length >= 2) {
+      display_text = messages[1].text;
+    }
   } catch (e) {
     console.error(e);
   }
+
+  if (fetch_response == null) {
+    res.send("error");
+    return;
+  }
+
+  const verb = action_value === "approve" ? "approved" : "rejected";
+
+  if (action_value === "approve") {
+    a_base('Feedback').update(record_id, {
+      "Display Text": display_text,
+      "Display": true
+    });
+  } else {
+    a_base('Feedback').update(record_id, {
+      "Display": false
+    });
+  }
+
+  let extra = "";
+
+  if (display_text !== "" && action_value === "approve") {
+    extra = " The text of the feedback was updated.";
+  }
+
+  const end_response = {blocks: blocks};
+  end_response.blocks[4] = {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "_This compliment has been " + verb + " by " + username + "." + extra + "_"
+    }
+  };
+  res.json(end_response);
 });
 
 module.exports = router;
