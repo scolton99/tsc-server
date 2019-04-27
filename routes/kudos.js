1f095359fb783a62287c4d79c5b2bead05a7393c
Spencer Colton <me@scolton.tech>
2019-04-27 15:41:09 -0500

    Parse payload as JSON from string

--- routes/kudos.js
+++ routes/kudos.js
@@ -5,7 +5,7 @@ var request = require('request-promise');
 router.post('/', async (req, res) => {
   console.dir(req.body);
 
-  const {channel: {id: channel}, message: {ts: message_ts}} = req.body.payload;
+  const {channel: {id: channel}, message: {ts: message_ts}} = JSON.parse(req.body.payload);
   const api_token = process.env.SLACK_API_TOKEN;
   try {
     const fetch_response = await request(`https://slack.com/api/conversations.replies?token=${api_token}&channel=${channel}&ts=${message_ts}`);
