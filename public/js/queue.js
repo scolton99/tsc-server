6a3bec3a4e56251f1da534c912a41acd0c06005c
Spencer Colton <me@scolton.tech>
2019-09-03 11:32:09 -0500

    Added nuke background
    Former-commit-id: 41b957740e508648e5f70598e7a46a72826ced7e

--- public/js/queue.js
+++ public/js/queue.js
@@ -8,14 +8,23 @@ const refresh_tickets = async () => {
     ticket_info.innerHTML = `<h1 class="title">There ${verb} ${num_tickets} ${noun} in the queue.</h1>`;
 
     const fire = document.getElementById("background-fire");
+    const nuke = document.getElementById("background-nuke");
 
-    if (typeof (num_tickets) === "number" && num_tickets >= 100)
+    if (typeof (num_tickets) === "number" && num_tickets >= 100 && num_tickets < 150)
       fire.classList.add("active");
     else
       fire.classList.remove("active");
 
+    if (typeof (num_tickets) === "number" && num_tickets >= 150)
+      nuke.classList.add("active");
+    else
+      nuke.classList.remove("active");
+
     if (fire.paused)
       fire.play();
+
+    if (nuke.paused)
+      nuke.play();
   });
 };
 
