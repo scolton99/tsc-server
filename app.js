bb8c99d0347fb7434d5e798a7c38b24bcaba0eff
Spencer Colton <me@scolton.tech>
2019-11-27 11:14:43 -0600

    Add App Engine proxy trust

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.set('trust proxy', true);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
