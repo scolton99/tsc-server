0432647c1a6c94f081b3a41596912f676baadc24
Spencer Colton <me@scolton.tech>
2019-12-11 07:09:00 -0600

    Fixing schedule...

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -3,4 +3,131 @@ date.setMinutes(date.getMinutes() + (5 - date.getMinutes() % 5));
 date.setSeconds(0);
 console.log(date.toString());
 
-window.setTimeout(() => (location.reload()), date.getTime() - (new Date()).getTime());
+const c_sv_d = document.querySelector("#now .supervisor");
+const c_1800_d = document.querySelector("#now .i1800");
+const c_lib_d = document.querySelector("#now .library");
+
+const n_sv_d = document.querySelector("#next .supervisor");
+const n_1800_d = document.querySelector("#next .i1800");
+const n_lib_d = document.querySelector("#next .library");
+
+const refresh_schedule = async () => {
+  const req_date = location.hash.substr(1);
+  let req_uri;
+  if (req_date === "")
+    req_uri = "/schedule/status"
+  else
+    req_uri = `/schedule/status?date=${req_date}`
+
+  const res_raw = await fetch(req_uri);
+  const res_json = await res_raw.json();
+
+  console.log(res_json);
+
+  while (n_sv_d.firstElementChild) {
+    n_sv_d.removeChild(n_sv_d.firstElementChild);
+  }
+
+  while (n_1800_d.firstElementChild) {
+    n_1800_d.removeChild(n_1800_d.firstElementChild);
+  }
+
+  while (n_lib_d.firstElementChild) {
+    n_lib_d.removeChild(n_lib_d.firstElementChild);
+  }
+
+  const ex_c_sv_ppl = Array.from(c_sv_d.children);
+  const ex_c_1800_ppl = Array.from(c_1800_d.children);
+  const ex_c_lib_ppl = Array.from(c_lib_d.children);
+
+  for (let per of ex_c_1800_ppl) {
+    if (!res_json.now.sch["1800 Consultant"].includes(per.textContent)) {
+      per.parentElement.removeChild(per);
+    } else {
+      res_json.now.sch["1800 Consultant"].splice(res_json.now.sch["1800 Consultant"].indexOf(per.textContent), 1);
+    }
+  }
+
+  for (let per of ex_c_lib_ppl) {
+    if (!res_json.now.sch["Library Consultant"].includes(per.textContent)) {
+      per.parentElement.removeChild(per);
+    } else {
+      res_json.now.sch["Library Consultant"].splice(res_json.now.sch["Library Consultant"].indexOf(per.textContent), 1);
+    }
+  }
+
+  for (let per of ex_c_sv_ppl) {
+    if (!res_json.now.sch["Consultant Supervisor"].includes(per.textContent)) {
+      per.parentElement.removeChild(per);
+    } else {
+      res_json.now.sch["Consultant Supervisor"].splice(res_json.now.sch["Consultant Supervisor"].indexOf(per.textContent), 1);
+    }
+  }
+
+  const st_func = e => {
+    e.currentTarget.classList.toggle("st");
+  };
+
+  for (let nn of res_json.now.sch["1800 Consultant"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.now.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    c_1800_d.appendChild(nd);
+  }
+
+  for (let nn of res_json.now.sch["Library Consultant"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.now.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    c_lib_d.appendChild(nd);
+  }
+
+  for (let nn of res_json.now.sch["Consultant Supervisor"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.now.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    c_sv_d.appendChild(nd);
+  }
+
+  for (let nn of res_json.next.sch["1800 Consultant"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.next.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    n_1800_d.appendChild(nd);
+  }
+
+  for (let nn of res_json.next.sch["Library Consultant"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.next.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    n_lib_d.appendChild(nd);
+  }
+
+  for (let nn of res_json.next.sch["Consultant Supervisor"]) {
+    const nd = document.createElement("span");
+    nd.textContent = nn;
+    if (res_json.next.trades[nn]) {
+      nd.classList.add("trade");
+    }
+    nd.addEventListener("click", st_func);
+    n_sv_d.appendChild(nd);
+  }
+};
+
+const refresh_interval = window.setTimeout(refresh_schedule, date.getTime() - (new Date()).getTime());
+refresh_schedule();
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const { google } = require('googleapis');
 const { Storage } = require('@google-cloud/storage');
 const IP = require('../util/IP');
+const request = require('request-promise-native');
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/calendar.events.readonly'];
+const SHIFT_TRADE_APP = process.env.SHIFT_TRADE_APP_URI;
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
 const authorize = async credentials => {
     const { client_secret, client_id, redirect_uris } = credentials.web;
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
     const storage = new Storage();
-    const token = (await storage.bucket('tss-support-center.appspot.com').file('private/token').download())[0];
+    
+    try {
+        const token = (await storage.bucket(BUCKET_NAME).file('private/token').download())[0];
 
-    if (token === null || token === "" || (typeof(token) === "object" && token.length === 0)) {
+        if (token === null || token === "" || (typeof(token) === "object" && token.length === 0)) {
+            throw new Error("Unauthorized");
+        }
+
+        oAuth2Client.setCredentials(JSON.parse(token));
+    
+        return oAuth2Client;
+    } catch (e) {
         const auth_url = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
             scope: SCOPES
@@ -25,23 +36,91 @@ const authorize = async credentials => {
 
         throw new Error("Unauthorized");
     }
-
-    oAuth2Client.setCredentials(JSON.parse(token));
-
-    return oAuth2Client;
 }
 
-router.get('/', async(req, res) => {
+const next_shift = date => {
+    if (date === null)
+        date = new Date();
+
+    const next = new Date(date.getTime());
+
+    const hrs = date.getHours();
+    const mins = date.getMinutes();
+
+    switch (hrs) {
+        case 7: {
+            next.setHours(9);
+            next.setMinutes(0);
+            break;
+        }
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15: {
+            if (mins < 30) {
+                next.setMinutes(30); 
+            } else {
+                next.setHours(hrs + 1);
+                next.setMinutes(0);
+            }
+
+            break;
+        }
+        case 16: {
+            if (mins < 30) {
+                next.setMinutes(30);
+            } else {
+                next.setHours(7);
+                next.setMinutes(45);
+                next.setDate(next.getDate() + 1);
+            }
+
+            break;
+        }
+        default: {
+            next.setHours(7);
+            next.setMinutes(45);
+            if (hrs >= 18)
+                next.setDate(next.getDate() + 1);
+        }
+    }
+
+    return next;
+}
+
+const gen_res_obj = (events, date) => {
+    const people = {"1800 Consultant": [], "Consultant Supervisor": [], "Library Consultant": []};
+
+    events.filter(e => {
+        return (new Date(e.start.dateTime) <= date) && (new Date(e.end.dateTime) > date);
+    }).forEach(e => {
+        if (e.summary.includes("1800 Consultant")) {
+            people["1800 Consultant"].push(/^(.*)1800 Consultant/.exec(e.summary)[1].trim());
+        } else if (e.summary.includes("Consultant Supervisor")) {
+            people["Consultant Supervisor"].push(/^(.*)Consultant Supervisor/.exec(e.summary)[1].trim());
+        } else if (e.summary.includes("Library Consultant")) {
+            people["Library Consultant"].push(/^(.*)Library Consultant/.exec(e.summary)[1].trim());
+        }
+    });
+
+    return people;
+};
+
+router.get('/status', async(req, res) => {
     if (!IP.isLocal(req.ip) && !IP.isNU(req.ip)) {
         console.log(`Refused schedule access to outside address ${req.ip}`);
         console.log(req.ips)
         return res.status(403).sendFile('forbidden.html', { root: global.root_dir + '/public' });
     }
 
-    const { date: mrn } = req.query;
+    let { date: mrn } = req.query;
 
     const storage = new Storage();
-    const bucket = storage.bucket('tss-support-center.appspot.com');
+    const bucket = storage.bucket(BUCKET_NAME);
 
     const secret = await bucket.file('private/credentials.json').download();
     const auth = await authorize(JSON.parse(secret));
@@ -52,6 +131,7 @@ router.get('/', async(req, res) => {
     ds.setMinutes(0);
 
     const de = mrn ? new Date(mrn) : new Date();
+    de.setDate(de.getDate() + 1);
     de.setHours(23);
     de.setMinutes(59);
 
@@ -60,29 +140,67 @@ router.get('/', async(req, res) => {
         timeMin: ds.toISOString(),
         timeMax: de.toISOString(),
         singleEvents: true
-    }, (err, results) => {
+    }, async (err, results) => {
         if (err) {
             console.error("The API returned an error: ", err);
             throw err;
         }
 
         const events = results.data.items;
-        const people = { "1800 Consultant": [], "Consultant Supervisor": [], "Library Consultant": [] };
 
-        events.filter(e => {
-            const rn = mrn ? new Date(mrn) : new Date();
-            return (new Date(e.start.dateTime) <= rn) && (new Date(e.end.dateTime) > rn);
-        }).forEach(e => {
-            if (e.summary.includes("1800 Consultant")) {
-                people["1800 Consultant"].push(/^(.*)1800 Consultant/.exec(e.summary)[1].trim());
-            } else if (e.summary.includes("Consultant Supervisor")) {
-                people["Consultant Supervisor"].push(/^(.*)Consultant Supervisor/.exec(e.summary)[1].trim());
-            } else if (e.summary.includes("Library Consultant")) {
-                people["Library Consultant"].push(/^(.*)Library Consultant/.exec(e.summary)[1].trim());
-            }
+        const rn = mrn ? new Date(mrn) : new Date();
+        const ltr_date = next_shift(rn);
+    
+        const now = gen_res_obj(events, rn);
+        const ltr = gen_res_obj(events, ltr_date);
+
+        const now_cons = [
+            ...now["1800 Consultant"],
+            ...now["Consultant Supervisor"],
+            ...now["Library Consultant"]
+        ];
+
+        const ltr_cons = [
+            ...ltr["1800 Consultant"],
+            ...ltr["Consultant Supervisor"],
+            ...ltr["Library Consultant"]
+        ];
+
+        const now_trades = await request({
+            uri: SHIFT_TRADE_APP,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: {date: rn.getTime(), cons: now_cons},
+            json: true,
+            simple: false,
+            followAllRedirects: true
         });
 
-        res.render('schedule', { people: people });
+        const ltr_trades = await request({
+            uri: SHIFT_TRADE_APP,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: {date: ltr_date.getTime(), cons: ltr_cons},
+            json: true,
+            simple: false,
+            followAllRedirects: true
+        });
+
+        res.json({
+            now: {
+                sch: now, 
+                trades: now_trades.cons
+            },
+            next: {
+                date: ltr_date.getTime(),
+                sch: ltr,
+                trades: ltr_trades.cons
+            }
+        });
     });
 });
 
@@ -90,7 +208,7 @@ router.get('/reauth', async(req, res) => {
     const code = req.query.code;
 
     const storage = new Storage();
-    const bucket = storage.bucket('tss-support-center.appspot.com');
+    const bucket = storage.bucket(BUCKET_NAME);
     const credentials_raw = await bucket.file('private/credentials.json').download();
 
     const credentials = JSON.parse(credentials_raw);
@@ -99,10 +217,20 @@ router.get('/reauth', async(req, res) => {
 
     const { tokens: token } = await oAuth2Client.getToken(code);
 
-    const file = storage.bucket('tss-support-center.appspot.com').file('private/token');
+    const file = storage.bucket(BUCKET_NAME).file('private/token');
     file.createWriteStream({ resumable: false }).end(JSON.stringify(token));
 
     res.redirect('/schedule');
+});
+
+router.get('/', (req, res) => {
+    if (!IP.isLocal(req.ip) && !IP.isNU(req.ip)) {
+        console.log(`Refused schedule access to outside address ${req.ip}`);
+        console.log(req.ips)
+        return res.status(403).sendFile('forbidden.html', { root: global.root_dir + '/public' });
+    }
+
+    res.render('schedule');
 });
 
 module.exports = router;
