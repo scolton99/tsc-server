67a0bfb577e2d0a9381a118db0214a7c46fca2ef
Spencer Colton <me@scolton.tech>
2020-04-14 20:25:43 -0400

    routes/assignment-group: migrate to Bent (see #13)

--- routes/assignment-group.js
+++ routes/assignment-group.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const request = require('request-promise-native');
+const bent = require('bent');
 const xml2js = require('xml2js');
 const FP = require('../util/FP');
 
 const fp_request = fs.readFileSync(__dirname + '/../assets/assignment_group_request.xml', { encoding: 'UTF-8' });
 const sep = " → ";
+
+const bent_string = bent('string', 'POST', 200, 301, 302);
 
 // Taken from https://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly
 const escape_string = str => {
@@ -52,52 +54,41 @@ router.get('/:group', async (req, res, next) => {
     .replace("{{GROUP_NAME}}", escape_string(group));
 
   try {
-    const response = await request({
-      method: 'POST',
-      uri: 'https://itsm-fp.northwestern.edu/MRcgi/MRWebServices.pl',
-      body: fp_request_auth,
-      resolveWithFullResponse: true
+    const response = await bent_string('https://itsm-fp.northwestern.edu/MRcgi/MRWebServices.pl', fp_request_auth);
+
+    xml2js.parseString(response, (err, result) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+
+      const items = result["SOAP-ENV:Envelope"]["SOAP-ENV:Body"][0]["namesp1:MRWebServices__searchResponse"][0]["return"][0]["item"];
+      let formatted_items = [];
+      const possible_categories = ["service__bfamily", "service", "category", "sub__ucategory"];
+
+      for(const item of items) {
+        const categories = [];
+
+        for (let possible_category of possible_categories)
+          if ("_" in item[possible_category][0])
+            categories.push(FP.unfix(item[possible_category][0]["_"]))
+
+        formatted_items.push({
+          category: categories.join(sep),
+          count: parseInt(item["count"][0]["_"])
+        });
+      }
+
+      const response_json = {
+        result: 'success',
+        group_name: FP.unfix(group),
+        stats: formatted_items
+      }
+      
+      res.json(response_json);
     });
-
-    if (response.statusCode >= 400) {
-      res.status(500).json({
-        result: 'failure',
-        message: response.body
-      });
-    } else {
-      xml2js.parseString(response.body, (err, result) => {
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
-
-        const items = result["SOAP-ENV:Envelope"]["SOAP-ENV:Body"][0]["namesp1:MRWebServices__searchResponse"][0]["return"][0]["item"];
-        let formatted_items = [];
-        const possible_categories = ["service__bfamily", "service", "category", "sub__ucategory"];
-
-        for(const item of items) {
-          const categories = [];
-
-          for (let possible_category of possible_categories)
-            if ("_" in item[possible_category][0])
-              categories.push(FP.unfix(item[possible_category][0]["_"]))
-
-          formatted_items.push({
-            category: categories.join(sep),
-            count: parseInt(item["count"][0]["_"])
-          });
-        }
-
-        const response_json = {
-          result: 'success',
-          group_name: FP.unfix(group),
-          stats: formatted_items
-        }
-        
-        res.json(response_json);
-      });
-    }
   } catch (e) {
+    console.error(e.message);
     res.status(500).json({
       result: 'failure',
       message: e.message
